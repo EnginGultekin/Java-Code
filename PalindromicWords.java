56e744372541fd6b85ac82e8862026de913f1167
Engin Gültekin <engingultekin1071@gmail.com>
2023-07-15 12:55:32 +0300

    Program to find palindrome words

---
+++ PalindromicWords.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PalindromicWords {
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    static boolean isPalindrome2(String str) {
+        StringBuilder reverse = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse.append(str.charAt(i));
+        }
+
+        return str.contentEquals(reverse.toString());
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+
+        System.out.println(isPalindrome(sentence));
+        System.out.println(isPalindrome2(sentence));
+    }
+}
